2c0b724aef360093ccd416fc6b56ed7452dbfeda
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-28 19:04:55 -0700

    Add OnMove function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,6 +9,11 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         
+    }
+
+    void OnMove (InputValue movementValue)
+    {
+   
     }
 
     // Update is called once per frame
