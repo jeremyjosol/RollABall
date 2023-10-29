ce04c85250a5db09e0ba4ff22d181850505efb48
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-28 19:06:34 -0700

    Assign Rigidbody value

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent <Rigidbody>(); 
     }
 
     void OnMove (InputValue movementValue)
