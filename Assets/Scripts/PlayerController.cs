ee0becb937727e795658f6f9c589d12bb7ac745c
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-28 19:09:00 -0700

    Add x & y movements + apply force to Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,18 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb; 
+    private float movementX;
+    private float movementY;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent <Rigidbody>(); 
+    }
+
+    private void FixedUpdate() 
+    {
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+        rb.AddForce(movement); 
     }
 
     void OnMove (InputValue movementValue)
